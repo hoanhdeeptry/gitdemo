6feab6312df8837790e60624883b545b9f3dedbe
Hoang Anh <adiep05@gmail.com>
2024-04-22 22:24:42 +0700

    update

---
+++ demo/jsBasics/script.js
@@ -0,0 +1,13 @@
+// Khai bao bien
+let fullName = 'Diep Hoang Anh';
+let age = 26;
+
+// Ham Built-in
+alert(fullName);
+alert(age);
+
+console.log(fullName);
+console.log(age);
+
+confirm('Xac nhan ban du tuoi!');
+prompt('Nhap do tuoi:');
