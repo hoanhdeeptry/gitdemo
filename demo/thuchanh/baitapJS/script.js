accff933f022e339d91494d3d37c7127b1c46ecb
Hoang Anh <adiep05@gmail.com>
2024-04-23 00:04:24 +0700

    update

--- demo/thuchanh/baitapJS/script.js
+++ demo/thuchanh/baitapJS/script.js
@@ -1,10 +1,31 @@
-let w = prompt('nhap chieu rong: ');
-let h = prompt('nhap chieu dai: ');
+// Bai 1
 
-const areaValue = (w + h) * 2;
-console.log( `chu vu hinh chu nhat la ${areaValue}` );
+// let w = prompt('nhap chieu rong: ');
+// let h = prompt('nhap chieu dai: ');
 
-const perimeterValue = w * h;
-console.log( `dien tich hinh chu nhat la ${perimeterValue}` );
+// const areaValue = (w + h) * 2;
+// console.log( `chu vu hinh chu nhat la ${areaValue}` );
+
+// const perimeterValue = w * h;
+// console.log( `dien tich hinh chu nhat la ${perimeterValue}` );
 
 
+// Bai 2
+
+// let S = prompt('Nhap so giay');
+// let H = (S - S%3600)/3600;
+// let M = ((S %3600)-(S % 3600) % 60)/60;
+
+// console.log(`ket qua la: ${H}h ${M}p`);
+
+
+// Bai 3
+
+// let a = prompt('Nhap co so: ');
+// let b = prompt('Luy thua: ');
+
+// const c = Math.pow(a,b);
+// alert(`ket qua la: ${c}`);
+
+
+// Bai 4
