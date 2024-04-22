a39152e41d173e6c481fe42b1523340a1b8aabf0
Hoang Anh <adiep05@gmail.com>
2024-04-22 21:45:50 +0700

    update

---
+++ demo/thuchanh/baitapJS/script.js
@@ -0,0 +1,8 @@
+let w = prompt('nhap chieu rong: ');
+let h = prompt('nhap chieu dai: ');
+
+const areaValue = (w + h) * 2;
+console.log( `chu vu hinh chu nhat la ${areaValue}` );
+
+const perimeterValue = w * h;
+console.log( `dien tich hinh chu nhat la ${perimeterValue}` );
